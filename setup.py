a7b8f4766b226f6405447ac75fba3c05e96b4eeb
PavloShutz <pavlo050706@gmail.com>
2023-01-29 19:28:18 +0200

    added setup tools for project

---
+++ setup.py
@@ -0,0 +1,17 @@
+"""Setup file for project english-lab."""
+
+
+from setuptools import find_packages, setup
+
+setup(
+    name='english-lab',
+    version='1.0.0',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'flask',
+        'flask-login',
+        'flask-sqlalchemy',
+        'flask-migrate'
+    ]
+)
